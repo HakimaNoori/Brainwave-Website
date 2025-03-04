03507545e472b5f10f771ec82f028dab291937b7
HakimaNoori <hakima.noori.1379@gmail.com>
2025-03-04 20:41:12 +0430

    add code

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Benefits = () => {
+    return (
+        <div>
+            <h2>Benefits</h2>
+            <p>Here are some benefits of using our service.</p>
+        </div>
+    );
+};
+
+export default Benefits;
